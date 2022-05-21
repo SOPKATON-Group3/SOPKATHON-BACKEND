ba0346a000b9023849f3140dd5f4c4e26ff1ff67
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-05-22 04:56:41 +0900

    test: controller test

--- src/test/letter.test.ts
+++ src/test/letter.test.ts
@@ -15,6 +15,8 @@ mongoose.Promise = global.Promise;
 let chai = require("chai");
 chai.should();
 
+const MONGODB_URL = "mongodb+srv://sopkathon3:sopkathon3@cluster0.imwo0.mongodb.net/?retryWrites=true&w=majority"
+
 const letter1 = new Letter({
     'nickname': '행복한 소설가',
     'contents': '훌륭한 소설가는 많지만 훌륭한 수필가는 드물다고. 이유는 간명하다. 그러니까 이번 책은 행복한 작가 공지영의 첫 번째 출사표라고 할 수 있다.',
